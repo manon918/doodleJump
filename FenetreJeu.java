98fb24eeb6e9eff3840b4673cc6fc56a9f9b1bec
manon918 <manon.salvado@insa-lyon.fr>
2020-02-27 17:37:29 +0100

    ajout du fond et du doodle immobile

--- FenetreJeu.java
+++ FenetreJeu.java
@@ -31,7 +31,7 @@ import java.util.LinkedList;
 		// Les Widgets à déclarer en dehors du constructeur
 
 		public FenetreJeu() {
-			this.setTitle("doodleJump ");
+			this.setTitle("DoodleJump ");
 			this.setSize(1000, 2000); // choisir taille initiale de la fenêtre
 			// Pour placer la fenêtre au centre de l'écran
 			//~ this.setLocationRelativeTo(null);
@@ -40,32 +40,37 @@ import java.util.LinkedList;
 			this.setResizable(false);
 			// Pour permettre la fermeture de la fenêtre lors de l'appui sur la croix rouge
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			//ajout fond
-
-			JLabel Fond = new JLabel("");
-			Fond.setBounds(0,0,1000,2000);
-			Fond.setIcon(new ImageIcon("C:\\Users\\manon\\IdeaProjects\\doodleJump\\src\\Fond.png"));
-			add(Fond);
 
 			//Ajout doodle
-			JLabel Etiquette = new JLabel("");
-			Etiquette.setBounds(250,250,500,500);
-			Etiquette.setIcon(new ImageIcon("C:\\Users\\manon\\IdeaProjects\\doodleJump\\src\\Doodle.png"));
-			add(Etiquette);
+			ImageIcon doodle = new ImageIcon("C:\\Users\\manon\\IdeaProjects\\doodleJump\\src\\Doodle.png");
+			//Redimensionnement du doodle
+			int Width = 100;
+			int Height = 100;
+			doodle = new ImageIcon(doodle.getImage().getScaledInstance(Width,Height,Image.SCALE_DEFAULT));
+			JLabel Doodle = new JLabel(doodle);
+			Doodle.setBounds(0,0,500,500);
+			Doodle.setLayout(null);
 
 			/**
-			 * Mon panneau Global
-			 */
+		 * Mon panneau Global
+		 */
 			JPanel panneauGlobal = new JPanel();
 			panneauGlobal.setBounds(0,0,1000,2000);
 			panneauGlobal.setLayout(null);
-			panneauGlobal.setBackground(Color.magenta);
+			add(panneauGlobal);
+			panneauGlobal.add(Doodle);
 
-			this.add(panneauGlobal);
-			// Pour rendre la fenêtre visible
+			//Redimensionnement de l'image de fond pour ajustement à la fenêtre
+			ImageIcon fond = new ImageIcon("C:\\Users\\manon\\IdeaProjects\\doodleJump\\src\\Fond.png");
+			int newWidth = panneauGlobal.getWidth() ;
+			int newHeight = panneauGlobal.getHeight();
+			fond = new ImageIcon(fond.getImage().getScaledInstance(newWidth,newHeight,Image.SCALE_DEFAULT));
+			JLabel Fond = new JLabel(fond);
+			Fond.setBounds(0,0,1000,2000);
+			panneauGlobal.add(Fond);
+
+
 			this.setVisible(true);
-
-
 		}
 	}
 
--- GestionFenetre.java
+++ GestionFenetre.java
@@ -1,7 +1,9 @@
+
 public class GestionFenetre {
     public static void main(String[] args){
         // Création de la fenêtre pour l'IHM
         FenetreJeu maFentreTest = new FenetreJeu();
     }
+
     }
 
