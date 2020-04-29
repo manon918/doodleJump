f29f8596cbc9b39dccdd169036b9ff084fde8500
marie1709 <61468219+marie1709@users.noreply.github.com>
2020-04-29 16:57:42 +0200

    Add files via upload

--- Palier.java
+++ Palier.java
@@ -6,11 +6,13 @@ public class Palier extends Element {
 
     protected int width;
     protected int height;
+	final int HEIGHT = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight();// la fenêtre s'adapte à la taille de l'écran ordinateur
+	final int WIDTH= HEIGHT/2; // initialisation largeur Fenetre de jeu
 
     public Palier(int x, int y, JLabel palier){
         super(x,y, palier);
-        this.width = 58;
-        this.height = 15;
+        this.width = (int)(HEIGHT/18);
+        this.height = (int)(WIDTH/29);
         palier.setBounds(x, y, width, height);
         palier.setLayout(null);
     }
